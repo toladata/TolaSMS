cf31ca51229185437515d0ff3f68f0a1beef6c88
Denis Neuling <denisneuling@gmail.com>
2017-09-05 18:21:54 +0200

    Extend urls.py to expose static resources properly

--- TolaSMS/urls.py
+++ TolaSMS/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rtropo.views import message_received
 
 
@@ -27,3 +28,5 @@ urlpatterns = [
     #twilio
     url(r'^backend/twilio/', include('rtwilio.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
